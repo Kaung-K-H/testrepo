bec498960f2c10f4689b59c759667b2af421f838
Kaung Khant Hein <40614799@live.napier.ac.uk>
2024-01-23 15:12:25 +0630

    build test and workflow 3

--- src/main/java/com/napier/sem/App.java
+++ src/main/java/com/napier/sem/App.java
@@ -4,5 +4,6 @@ public class App {
     public static void main(String[] args) {
         System.out.println("Hello there!");
         System.out.println("Hey bastard");
+        System.out.print("Just wanna know if this would work");
     }
 }
